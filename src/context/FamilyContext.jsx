12799aa98ca800bf54dc12a83bd1f6eb8143816b
Danielfiki <danielundurraga.r@gmail.com>
2026-04-25 20:38:14 -0400

    debug: log family_members result in FamilyContext

--- src/context/FamilyContext.jsx
+++ src/context/FamilyContext.jsx
@@ -40,7 +40,7 @@ export function FamilyProvider({ children }) {
           .maybeSingle(),
       ])
 
-      if (memberError) console.error('family_members error:', memberError)
+      console.log('[FamilyContext] family_members row:', { membership, memberError, userId: user.id })
 
       if (membership?.family_id) {
         // Round 2: find the other member in this family
